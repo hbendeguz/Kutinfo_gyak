9929eaf5adcafc29b2df2acbe92bfd432335dac4
bendeguz <bendeguz.horvath@gmail.com>
2018-05-15 10:39:01 +0200

    writes to file

--- project/popdin.cpp
+++ project/popdin.cpp
@@ -16,7 +16,15 @@ double c = 0.004;//rate of ...
 double d = 0.9; // rate of ...
 double K = 800; // capacity 
 double s = 2500;
+double alpha = 2.3;
 
+double beta =0.02;
+
+double r1 = 0.5;
+double r2 = 0.5;
+double k1 = 20;
+double k2 = 20;
+// defining the functions:
 
 double* LotkaVolterra(double nr, double nf){
     double np[2];
@@ -39,12 +47,20 @@ double* CapacityLV(double nr, double nf){
     return np;
 }
 
+double* SaturationLV(double nr, double nf){
+    double np[2];
+    np[0] = a*nr-b*nr*nf/(1+nr/s);
+    np[1] = c*nr*nf/(1+nr/s)-d*nf;
+    return np;
+}
+
 double* RealisticLV(double nr, double nf){
     double np[2];
     np[0] = a*nr*(1-nr/K)-b*nr*nf/(1+nr/s);
     np[1] = c*nr*nf/(1+nr/s)-d*nf;
     return np;
 }
+
 
 double* eulerStep(double x, double tau, double y, function<double* (double,double)> f1){
     x = x+tau*f1(x, y)[0];
@@ -59,6 +75,17 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     
     cout << "Let the simulation begin!\n";
+    for(int i = 0; i<100000; i++){
+        double x = nr;
+        //  nr =eulerStep(nr, 0.00001, nf, Logistic)[0];
+        //  nf =eulerStep(x, 0.00001, nf, Logistic)[1];
+        
+        nr =RK4Step(nr, 0.00001, nf, Logistic)[0];
+        nf =RK4Step(x, 0.00001, nf, Logistic)[1];
+        file << x/K <<'\t'<<nf << '\n';
+    }
+    file.close();
+    cout << "Finished"<< endl;
 
     return 0;
 }
